41d454d9a41ec2e938a9e1de978924009934f199
Gautham Mahadevan <128295307+gauthamgtg@users.noreply.github.com>
2024-08-17 17:14:24 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,17 +5,17 @@ from english_words import get_english_words_set
 st.set_page_config(page_title="Hangman Word Guesser", layout="centered")
 st.markdown("<h1 style='text-align: center; color: #FF5733;'>Hangman Word Guesser</h1>", unsafe_allow_html=True)
 
-# Display social links with icons
+# Display social links with updated icons
 st.markdown("""
 <div style='text-align: center;'>
     <a href='https://github.com/gauthamgtg' target='_blank'>
-        <img src='https://image.flaticon.com/icons/png/512/25/25231.png' width='40' style='margin: 0 15px;'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' width='40' style='margin: 0 15px;'>
     </a>
     <a href='https://gauthamgtg.github.io/portfolio/' target='_blank'>
-        <img src='https://image.flaticon.com/icons/png/512/1006/1006771.png' width='40' style='margin: 0 15px;'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/6/62/Portfolio%28light%29.svg' width='40' style='margin: 0 15px;'>
     </a>
     <a href='https://linkedin.com/in/gautham-mahadevan' target='_blank'>
-        <img src='https://image.flaticon.com/icons/png/512/174/174857.png' width='40' style='margin: 0 15px;'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo_2019.svg' width='40' style='margin: 0 15px;'>
     </a>
 </div>
 """, unsafe_allow_html=True)
@@ -38,31 +38,42 @@ def validate_excluded_letters(included_letters, excluded_letters):
 # Slider to choose word length
 word_length = st.slider("How many characters does the word have?", 2, 28, 5)
 
+# Initialize session state
+if 'letter_inputs' not in st.session_state:
+    st.session_state.letter_inputs = [""] * word_length
+if 'excluded_letters' not in st.session_state:
+    st.session_state.excluded_letters = []
+
 # Display input boxes side by side
 st.markdown("<h4 style='color: #33CFFF;'>Enter the known letters below:</h4>", unsafe_allow_html=True)
 cols = st.columns(word_length)
-letter_inputs = [cols[i].text_input(f"Letter {i+1}", "", max_chars=1, key=f"letter_{i}") for i in range(word_length)]
+for i in range(word_length):
+    st.session_state.letter_inputs[i] = cols[i].text_input(f"Letter {i+1}", st.session_state.letter_inputs[i], max_chars=1, key=f"letter_{i}")
 
 # User input for excluded letters
 st.markdown("<h4 style='color: #33CFFF;'>Enter letters that are not in the word:</h4>", unsafe_allow_html=True)
-excluded_letters_input = st.text_input("Excluded letters (separated by commas):").replace(" ", "").lower()
-excluded_letters = [letter for letter in excluded_letters_input if letter.isalpha()]
+excluded_letters_input = st.text_input("Excluded letters (separated by commas):", value=", ".join(st.session_state.excluded_letters)).lower()
+st.session_state.excluded_letters = [letter for letter in excluded_letters_input if letter.isalpha()]
 
 # Check function
-if st.button("Check"):
-    if validate_excluded_letters(letter_inputs, excluded_letters):
-        pattern = "".join([letter if letter else "_" for letter in letter_inputs])
-        # Word guessing logic
-        possible_words = [word for word in web2lowerset if len(word) == word_length and all(
-            (c1 == c2 or c2 == "_") and c1 not in excluded_letters for c1, c2 in zip(word, pattern)
-        )]
+col1, col2 = st.columns([4, 1])
+with col1:
+    if st.button("Check"):
+        if validate_excluded_letters(st.session_state.letter_inputs, st.session_state.excluded_letters):
+            pattern = "".join([letter if letter else "_" for letter in st.session_state.letter_inputs])
+            # Word guessing logic
+            possible_words = [word for word in web2lowerset if len(word) == word_length and all(
+                (c1 == c2 or c2 == "_") and c1 not in st.session_state.excluded_letters for c1, c2 in zip(word, pattern)
+            )]
 
-        if possible_words:
-            st.markdown("<h4 style='color: #28a745;'>Possible words:</h4>", unsafe_allow_html=True)
-            st.write(possible_words)
-        else:
-            st.markdown("<h4 style='color: #dc3545;'>No possible words found for your input.</h4>", unsafe_allow_html=True)
+            if possible_words:
+                st.markdown("<h4 style='color: #28a745;'>Possible words:</h4>", unsafe_allow_html=True)
+                st.write(possible_words)
+            else:
+                st.markdown("<h4 style='color: #dc3545;'>No possible words found for your input.</h4>", unsafe_allow_html=True)
 
-# Reset button to clear inputs
-if st.button("Reset"):
-    st.experimental_rerun()
+with col2:
+    if st.button("Reset"):
+        st.session_state.letter_inputs = [""] * word_length
+        st.session_state.excluded_letters = []
+        st.experimental_rerun()
